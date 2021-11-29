6e2dbb469b7b8eb953364cdedc03c96c03e786c5
ysy-aha <yshy360122@qq.com>
2021-11-29 22:24:58 +0800

    1. mybatis的逆向工程

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 瞎写的一些小东西
 
 1. tools-mysql，简单的Java程序，直接运行就可以了，根据给出的模板excel，生成mysql的DDL语句
+2. mybatis-generator，mybatis的逆向工程
---
+++ mybatis-generator/src/test/java/pers/yshy/mybatis/MybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.yshy.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
